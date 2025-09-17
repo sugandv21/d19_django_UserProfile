4e5dca87fb7e3556300dcf7e234e04614098ae7c
sugandv21 <sdvsuganya@gmail.com>
2025-09-17 11:54:01 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-40981_)b-!#klz5clp4phf#^)obff&q2dehf53fwqh92@+putn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -146,3 +146,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
